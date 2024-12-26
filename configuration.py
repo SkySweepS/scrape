d28d42896277ed34216491f56a8215ba926dec52
Svetlin Petkov <S.petkov991@icloud.com>
2024-12-26 18:16:22 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -16,10 +16,10 @@ FILE_NAME = "fibank_branches.xlsx"  # Name of the saved Excel file
 SCRAPING_URL = "https://my.fibank.bg/EBank/public/offices"  # URL to scrape data
 
 # Email Configuration
-SENDER_EMAIL = "absulutbg@gmail.com"
-RECIPIENT_EMAIL = "s.petkov991@icloud.com"
+SENDER_EMAIL = "your_email@gmail.com"
+RECIPIENT_EMAIL = "recipent_email@gmail.com"
 EMAIL_SUBJECT = "Fibank Branches Data"
 EMAIL_BODY = "Here is the Excel file with the Fibank branch information."
 
 # Sensitive Information
-EMAIL_PASSWORD = os.getenv("PASSWORD")  # Loaded securely from .env file
+EMAIL_PASSWORD = os.getenv("PASSWORD")
